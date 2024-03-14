1a22ab7cb0e2b8b933ff9a116f7a97a7d772cdb3
eric <e.muller3@yahoo.fr>
2024-03-14 14:02:58 +0100

    nav

--- src/Views/parts/footer.php
+++ src/Views/parts/footer.php
@@ -1,8 +1,13 @@
-<footer class="bg-body-tertiary text-center text-lg-start mt-5">
+<footer class="bg-body-tertiary text-center text-lg-start fixed-bottom">
+<div class="container">
+    <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2024 Rico
+        © 2024 Copyright: Rico
+       
     </div>
+    <!-- Copyright -->
 </footer>
+</div>
 </body>
-
-</html>
+ 
+</html
